52f065a8df2d21cd5684078b29f1deb4b657f608
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-08-07 14:42:12 -0700

    Setup file scaffolding for authRoute.js

---
+++ lab-eddie/route/auth-route.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const mongoose = require('mongoose');
+const creatError = require('-http-errors');
+const debug = require('debug')('app: auth-route')
+
+authRoute = new Router();
+
+authRoute.post('/api/signup', function(req, res, next) {
+  debug('POST /api/signup');
+});
+
+authRoute.get('/api/signin', function(req, res, next) {
+  debug('GET /api/signin');
+});
