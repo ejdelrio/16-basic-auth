ffe069758f48df7b33ee3edceb4273d046761652
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-08-07 16:08:32 -0700

    Wrote put for for userAuth

--- lab-eddie/route/auth-route.js
+++ lab-eddie/route/auth-route.js
@@ -2,16 +2,30 @@
 
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
-const mongoose = require('mongoose');
 const creatError = require('-http-errors');
-const debug = require('debug')('app: auth-route')
+const debug = require('debug')('app: auth-route');
+const basicAuth = require('../lib/basic-auth.js');
 
 authRoute = new Router();
 
 authRoute.post('/api/signup', jsonParser, function(req, res, next) {
   debug('POST /api/signup');
+
+  let passWord = req.body.passWord;
+  delete req.body.passWord;
+
+  let user = new User(req.body);
+
+  user.generatePasswordHash(passWord)
+  .then(user => user.save())
+  .then(user => user.generateToken())
+  .then(token => res.send(token))
+  .catch(next);
+
 });
 
-authRoute.get('/api/signin', function(req, res, next) {
+authRoute.get('/api/signin',basicAuth , function(req, res, next) {
   debug('GET /api/signin');
+
+
 });
